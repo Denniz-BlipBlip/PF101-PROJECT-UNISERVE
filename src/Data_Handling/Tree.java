a5e18886e27daa95a1f5081c26a94deecadae8d4
Denniz <gabrieldenniz@gmail.com>
2026-09-14 06:08:47 +0800

    added balance, height, update to tree

--- src/Data_Handling/Tree.java
+++ src/Data_Handling/Tree.java
@@ -3,10 +3,33 @@ import Users.Data;
 
 public class Tree 
 {
-  private int balance_factor(Node node){return !this.is_empty(node)?node.get_height():0;}
-  private int height(Node node){return !this.is_empty(node)?node.get_height():0;}
-  public Tree(){this.root=null;}
-  
+
+  private void uodate_height(Node node)
+  {
+    node.set_height(1+this.get_max_node(node));
+  }
+
+  private int get_max_node(Node node)
+  {
+    return Math.max(this.height(node.get_left()),this.height(node.get_right()));
+  }
+
+  private int balance_factor(Node node)
+  {
+    return !this.is_empty(node)?this.get_difference(node):0;
+  }
+
+  private int get_difference(Node node)
+  {
+    return this.height(node.get_left())-this.height(node.get_right());
+  }
+
+  private int height(Node node)
+  {
+    return !this.is_empty(node)?node.get_height():0;
+  }
+
   private boolean is_empty(Node node){return node==null;}
+  public Tree(){this.root=null;}
   private Node root;
 }
